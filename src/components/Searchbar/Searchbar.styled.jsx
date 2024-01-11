f79bbc675425f9a9db8828a64f37bd9131f70b7d
Lena <elenazyamileva@gmail.com>
2024-01-12 01:04:21 +0200

    change

---
+++ src/components/Searchbar/Searchbar.styled.jsx
@@ -0,0 +1,72 @@
+import styled from '@emotion/styled';
+
+export const Divv = styled.div`
+  margin-bottom: 25px;
+  padding: 20px;
+  min-width: 100%;
+
+  background-color: var(--bg);
+
+  box-shadow: var(--main-shadow);
+`;
+
+/* min-width: 100%;
+
+  background-color: var(--bg);
+
+  box-shadow: var(--main-shadow); */
+
+// export const SearchForm = styled.form`
+//   display: flex;
+//   align-items: center;
+//   width: 100%;
+//   max-width: 600px;
+//   background-color: #fff;
+//   border-radius: 3px;
+//   overflow: hidden;
+// `;
+
+// export const SearchFormButton = styled.button`
+//   display: inline-block;
+//   width: 48px;
+//   height: 48px;
+//   border: 0;
+//   background-image: url('https://image.flaticon.com/icons/svg/149/149852.svg');
+//   background-size: 40%;
+//   background-repeat: no-repeat;
+//   background-position: center;
+//   opacity: 0.6;
+//   transition: opacity 250ms cubic-bezier(0.4, 0, 0.2, 1);
+//   cursor: pointer;
+//   outline: none;
+//   :hover {
+//     opacity: 1;
+//   }
+// `;
+
+// export const ButtonLabel = styled.label`
+//   position: absolute;
+//   width: 1px;
+//   height: 1px;
+//   padding: 0;
+//   overflow: hidden;
+//   clip: rect(0, 0, 0, 0);
+//   white-space: nowrap;
+//   clip-path: inset(50%);
+//   border: 0;
+// `;
+
+// export const SearchFormInput = styled.input`
+//   display: inline-block;
+//   width: 100%;
+//   /* font: inherit; */
+//   font-size: 20px;
+//   border: none;
+//   outline: none;
+//   padding-left: 4px;
+//   padding-right: 4px;
+//   /* ::placeholder {
+//   font: inherit;
+//   font-size: 18px;
+// } */
+// `;
